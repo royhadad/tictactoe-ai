f6396b6025ba82ee7e59c760dc5a31051c65e89a
royhadad98 <roy@roundtrip.co.il>
2021-05-26 19:42:56 +0300

    added more ui and logic to the game

--- pages/game/Square/Square.tsx
+++ pages/game/Square/Square.tsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useContext} from "react";
 import createClasses from './style';
-import {OnClickSquare} from "../game";
+import {GameContext, getSquareStateByLocation} from "../game";
 import {Square as SquareStates, SquareLocation} from 'generic-min-max/dist/implementations/TicTacToe'
+import noop from 'lodash/noop';
 
 interface Props {
-    state: SquareStates;
     location: SquareLocation;
-    onClick: OnClickSquare;
 }
+
+const getSquareContent = (squareState: SquareStates): string => {
+    if (squareState === SquareStates.X) {
+        return 'X';
+    } else if (squareState === SquareStates.O) {
+        return 'O';
+    } else {
+        return '';
+    }
+};
 
 const Square: React.FC<Props> = (props: Props) => {
     const classes = createClasses();
+    const {gameState, onClickSquare} = useContext(GameContext);
+    const squareState = getSquareStateByLocation(gameState, props.location);
 
     return (
-        <div onClick={() => props.onClick(props.location)} className={classes.wrapper}>
+        <div
+            onClick={squareState === SquareStates.Empty ? () => onClickSquare(props.location) : noop}
+            className={classes.wrapper}
+        >
+            {getSquareContent(squareState)}
         </div>
     )
 }
--- pages/game/game.tsx
+++ pages/game/game.tsx
@@ -1,40 +1,76 @@
 import createClasses from './style';
 import Square from "./Square";
-import {useState} from "react";
-import {SquareLocation} from "generic-min-max/dist/implementations/TicTacToe";
+import {createContext, useState} from "react";
+import {
+    Board,
+    Square as SquareState,
+    SquareLocation,
+    TicTacToeState
+} from "generic-min-max/dist/implementations/TicTacToe";
 
 export type OnClickSquare = (squareLocation: SquareLocation) => void;
+const onClickSquareDefaultValue: OnClickSquare = (squareLocation => {
+})
+
+export const getSquareStateByLocation = (gameState: TicTacToeState, location: SquareLocation): SquareState => {
+    return (gameState.board[location[0]][location[1]])
+};
+
+const getEmptyBoard = (): Board => ([
+    [SquareState.Empty, SquareState.Empty, SquareState.Empty],
+    [SquareState.Empty, SquareState.Empty, SquareState.Empty],
+    [SquareState.Empty, SquareState.Empty, SquareState.Empty]
+]);
+
+const getNewGame = (): TicTacToeState => ({
+    player1Turn: true,
+    board: getEmptyBoard()
+})
+
+export const GameContext = createContext<{ gameState: TicTacToeState, onClickSquare: OnClickSquare }>({
+    gameState: getNewGame(),
+    onClickSquare: onClickSquareDefaultValue
+});
+
 
 function Game() {
     const classes = createClasses();
 
-    const gameState = useState()
+    const [gameState, setGameState] = useState(getNewGame());
 
     const onClickSquare: OnClickSquare = () => {
 
     }
 
+
     return (
-        <div>
-            <div className={classes.wrapper}>this is the tic tac toe game</div>
+        <GameContext.Provider
+            value={{
+                gameState: gameState,
+                onClickSquare
+            }}
+        >
             <div>
+                <div className={classes.wrapper}>this is the tic tac toe game</div>
                 <div>
-                    <Square onClick={onClickSquare} location={[0, 0]} state={}/>
-                    <Square onClick={onClickSquare} location={[0, 1]}/>
-                    <Square onClick={onClickSquare} location={[0, 2]}/>
-                </div>
-                <div>
-                    <Square onClick={onClickSquare} location={[1, 0]}/>
-                    <Square onClick={onClickSquare} location={[1, 1]}/>
-                    <Square onClick={onClickSquare} location={[1, 2]}/>
-                </div>
-                <div>
-                    <Square onClick={onClickSquare} location={[2, 0]}/>
-                    <Square onClick={onClickSquare} location={[2, 1]}/>
-                    <Square onClick={onClickSquare} location={[2, 2]}/>
+                    <div>
+                        <Square location={[0, 0]}/>
+                        <Square location={[0, 1]}/>
+                        <Square location={[0, 2]}/>
+                    </div>
+                    <div>
+                        <Square location={[1, 0]}/>
+                        <Square location={[1, 1]}/>
+                        <Square location={[1, 2]}/>
+                    </div>
+                    <div>
+                        <Square location={[2, 0]}/>
+                        <Square location={[2, 1]}/>
+                        <Square location={[2, 2]}/>
+                    </div>
                 </div>
             </div>
-        </div>)
+        </GameContext.Provider>)
 }
 
 export default Game;
